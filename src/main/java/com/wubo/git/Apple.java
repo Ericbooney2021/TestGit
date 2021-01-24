241e82e8745f774c395ffe80e63603a30cf50ea9
eric <doctor_eric@163.com>
2021-01-24 09:46:11 +0800

    My first commit in IDEA

---
+++ src/main/java/com/wubo/git/Apple.java
@@ -0,0 +1,12 @@
+package com.wubo.git;
+
+/**
+ * @author Eric
+ * @version 1.0
+ * @className com.wubo.git.Apple
+ * @description TODO
+ * @date 2021-01-23 20:10:09
+ */
+
+public class Apple {
+}
---
+++ src/test/java/com/wubo/test/GoodTest.java
@@ -0,0 +1,12 @@
+package com.wubo.test;
+
+/**
+ * @author Eric
+ * @version 1.0
+ * @className com.wubo.test.GoodTest
+ * @description TODO
+ * @date 2021-01-23 20:11:44
+ */
+
+public class GoodTest {
+}
